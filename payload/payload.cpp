d566f16c784a2923591ecc27bd7a293bce41bead
prospect <kilijames051@gmail.com>
2025-11-16 02:16:30 +0000

    Add files via upload

---
+++ payload/payload.cpp
@@ -0,0 +1,5 @@
+#include "payload.h"
+
+const unsigned char g_DllPayload[1] = { 0x00 };
+
+const size_t g_DllPayloadSize = 0;
---
+++ payload/payload.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <cstddef>
+
+extern const unsigned char g_DllPayload[];
+extern const size_t g_DllPayloadSize;
+
